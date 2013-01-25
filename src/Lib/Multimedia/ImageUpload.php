77f5e1b2efe5874ee0fe7b80787ac081cb3ee258
Sohel Rana <me.sohelrana@gmail.com>
2013-01-26 04:35:04 +0600

    Image Upload object has been created in usage directory

--- src/Lib/Multimedia/ImageUpload.php
+++ src/Lib/Multimedia/ImageUpload.php
@@ -3,5 +3,22 @@
  * Imaage Upload Class
  */
 class ImageUpload{
-
+    public function ImgUpload(){
+        if(isset($_POST['submit']))
+        {
+            if(!empty($_FILES['ImgName']['name']))
+            {
+                if(move_uploaded_file($_FILES['ImgName']['tmp_name'],$path."/".$_FILES['ImgName']['name'])){
+                    echo 'ok';
+                }else{
+                    echo 'mm';
+                }
+                // $ImgName=$_FILES['ImgName']['name'];
+            }
+            else
+            {
+                 return 'You do not select any file';
+            }
+        }
+    }
 }
--- usage/Multimedia/ImageUpload.php
+++ usage/Multimedia/ImageUpload.php
@@ -1,5 +1,18 @@
-<?php
-require "../Multimedia/ImageUpload.php";
-
-//Create ImageUpload Object
-$obj=new ImageUpload();
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Image Upload Function Usage</title>
+    </head>
+    <body>
+        <?php
+        require_once "../../src/Lib/Multimedia/ImageUpload.php";
+        //Create ImageUpload Object
+        $obj=new ImageUpload();
+        echo $obj->ImgUpload();
+        ?>
+        <form action="?click=upload" method="post" enctype="multipart/form-data">
+            <input type="file" name="ImgName"/>
+            <input type="submit" name="submit" value="SUBMIT"/>
+        </form>
+    </body>
+</html>

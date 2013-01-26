596b16c4494169eeba8cbbb1c45228c4752c55bc
Shaharia Azam <shaharia.azam@gmail.com>
2013-01-26 06:25:34 +0600

    Just code re-formatting

--- usage/Multimedia/ImageUpload.php
+++ usage/Multimedia/ImageUpload.php
@@ -1,18 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <title>Image Upload Function Usage</title>
-    </head>
-    <body>
-        <?php
-        require_once "../../src/Lib/Multimedia/ImageUpload.php";
-        //Create ImageUpload Object
-        $obj=new ImageUpload();
-        echo $obj->ImgUpload();
-        ?>
-        <form action="?click=upload" method="post" enctype="multipart/form-data">
-            <input type="file" name="ImgName"/>
-            <input type="submit" name="submit" value="SUBMIT"/>
-        </form>
-    </body>
+<head>
+    <title>Image Upload Function Usage</title>
+</head>
+<body>
+<?php
+require_once "../../src/Lib/Multimedia/ImageUpload.php";
+//Create ImageUpload Object
+$obj = new ImageUpload();
+echo $obj->ImgUpload();
+?>
+<form action="?click=upload" method="post" enctype="multipart/form-data">
+    <input type="file" name="ImgName"/>
+    <input type="submit" name="submit" value="SUBMIT"/>
+</form>
+</body>
 </html>
